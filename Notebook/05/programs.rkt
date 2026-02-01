e7c0c65c8712ee237a8aa41884025bf61401167c
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2026-01-31 22:47:25 -0500

    Programs from chapter 5

---
+++ Notebook/05/programs.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(define (g n)
+  (if (zero? n)
+      0
+      (- n (g (g (sub1 n))))))
+
+; https://arxiv.org/pdf/1509.02479
+(define (flip n)
+  (case n
+    [(0)   0]
+    [(1 2) 1]
+    [(3)   2]
+    [else  (- (add1 n) (flip (add1 (flip (sub1 n)))))]))
+
+(define (h n)
+  (if (zero? n)
+      0
+      (- n (h (h (h (sub1 n)))))))
+
+(define (q n)
+  (if (<= n 2)
+      1
+      (+ (q (- n (q (sub1 n))))
+         (q (- n (q (- n 2)))))))
+
+(define (experiment f n)
+  (for/list ([i (in-range n)])
+    (let ((i (add1 i)))
+      (cons i (f i)))))
