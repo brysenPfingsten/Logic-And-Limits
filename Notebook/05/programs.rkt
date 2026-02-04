5d66feaebea4a925d1b1dcabd42e21ee68e22a23
brysenPfingsten <brysen.pfingsten@gmail.com>
2026-02-04 12:21:13 -0500

    F and M programs

--- Notebook/05/programs.rkt
+++ Notebook/05/programs.rkt
@@ -8,10 +8,10 @@
 ; https://arxiv.org/pdf/1509.02479
 (define (flip n)
   (case n
-    [(0)   0]
+    [(0) 0]
     [(1 2) 1]
-    [(3)   2]
-    [else  (- (add1 n) (flip (add1 (flip (sub1 n)))))]))
+    [(3) 2]
+    [else (- (add1 n) (flip (add1 (flip (sub1 n)))))]))
 
 (define (h n)
   (if (zero? n)
@@ -21,10 +21,18 @@
 (define (q n)
   (if (<= n 2)
       1
-      (+ (q (- n (q (sub1 n))))
-         (q (- n (q (- n 2)))))))
+      (+ (q (- n (q (sub1 n)))) (q (- n (q (- n 2)))))))
 
 (define (experiment f n)
   (for/list ([i (in-range n)])
-    (let ((i (add1 i)))
-      (cons i (f i)))))
+    (let ([i (add1 i)]) (cons i (f i)))))
+
+(define (F n)
+  (if (<= n 0)
+      1
+      (- n (M (F (sub1 n))))))
+
+(define (M n)
+  (if (<= n 0)
+      0
+      (- n (F (M (sub1 n))))))
